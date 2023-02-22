edbe37898c2c1106e16063f1b674e58593e19aa7
qwemre <126076587+qwemre@users.noreply.github.com>
2023-02-22 17:00:02 +0300

    Add files via upload
    console arka plan renk değişimi ve yazı rengi değişimi

---
+++ ConsoleApp39/ConsoleApp39/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("consol arka plan rengini istediğiniz gibi yapınız (red-yellow-blue)");
+string alınan = Console.ReadLine();
+if (alınan == "red")
+{
+    Console.BackgroundColor = ConsoleColor.Red;
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("alınan değeri kırmızı olarak seçtiniz");
+
+}
+else if (alınan == "yellow")
+{
+    Console.BackgroundColor = ConsoleColor.Yellow;
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("alınan değeri sarı olarak seçtiniz");
+
+}
+else if (alınan == "blue")
+{
+    Console.BackgroundColor = ConsoleColor.Blue;
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Black;
+    Console.WriteLine("alınan değeri mavi olarak seçtiniz");
+}
+else
+{
+    Console.WriteLine("renk seçimi yapmadınız");
+}
